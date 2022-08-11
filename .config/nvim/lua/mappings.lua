0311c9428ab121d6995810f26a751609ebee38a6
kissy24 <kissy105808j@gmail.com>
2022-08-12 00:57:15 +0900

    change lua scripts

---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,9 @@
+-- Exit insert mode with jj
+vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+vim.keymap.set("i", "っj", "<ESC>", { silent = true })
+
+-- Moving the pane
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,7 @@
+vim.cmd [[packadd packer.nvim]]
+
+require 'packer'.startup(function()
+    -- ColorScheme
+    use "EdenEast/nightfox.nvim"
+
+end)
