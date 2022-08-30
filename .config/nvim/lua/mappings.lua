8e9d6a36422199189d94ee58c260154b90f4d419
kissy24 <12809711+kissy24@users.noreply.github.com>
2022-08-31 00:19:28 +0900

    update/nvim settings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -16,3 +16,6 @@ vim.keymap.set("n", "ff", "<cmd>lua require('telescope.builtin').find_files()<cr
 vim.keymap.set("n", "fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
 vim.keymap.set("n", "fb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
 vim.keymap.set("n", "fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>")
+
+-- markdown-pewview
+vim.keymap.set("n", "<C-p>", "<Plug>MarkdownPreviewToggle")
