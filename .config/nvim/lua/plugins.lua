d964c71bffca4f7529b8f582301f0cffa3b5ed9a
kissy24 <kissy105808j@gmail.com>
2022-08-31 00:19:28 +0900

    update/nvim settings

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,31 +1,43 @@
 vim.cmd [[packadd packer.nvim]]
 
 require "packer".startup(function()
-    use { "wbthomason/packer.nvim", opt = true }
+    use({ "wbthomason/packer.nvim", opt = true })
 
     -- LSP
-    use {'neoclide/coc.nvim', branch = 'release'}
+    use({'neoclide/coc.nvim', branch = 'release'})
 
     -- ColorScheme
-    use "EdenEast/nightfox.nvim"
+    use("EdenEast/nightfox.nvim")
 
     -- FileExplorer
-    use {
+    use({
         "kyazdani42/nvim-tree.lua",
         requires = { "kyazdani42/nvim-web-devicons" },
         tag = "nightly"
-    }
+    })
 
     -- FuzzyFinder
-    use {
+    use({
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
         requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+    })
 
     -- Statusline
     use({
         "nvim-lualine/lualine.nvim",
         after = colorscheme,
         requires = { "kyazdani42/nvim-web-devicons", opt = true }
+    })
+
+    use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+    })
+
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = "cd app && npm install",
+        setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+        ft = { "markdown" }
     })
 end)
