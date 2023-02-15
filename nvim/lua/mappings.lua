ec9e9f6c0388d126f066c32fc16d1c1f2218a53c
kissy24 <kissy105808j@gmail.com>
2023-02-15 23:56:13 +0900

    update nvim mapping

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -9,13 +9,13 @@ vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 -- terminal mode
-vim.keymap.set("n", "tx", "<cmd>belowright new<CR><cmd>terminal<CR>", { silent = true})
+vim.keymap.set("n", "tm", "<cmd>belowright new<CR><cmd>terminal<CR>", { silent = true})
 vim.cmd [[autocmd TermOpen * :startinsert]]
 vim.cmd [[autocmd TermOpen * setlocal norelativenumber]]
 vim.cmd [[autocmd TermOpen * setlocal nonumber]]
 
 -- nvim-tree
-vim.keymap.set("n", "<A-e>", ":NvimTreeToggle<cr>", { silent = true })
+vim.keymap.set("n", "tr", ":NvimTreeToggle<cr>", { silent = true })
 
 -- Telescope
 vim.keymap.set("n", "ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
