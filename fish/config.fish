7cc9ade7980693f95647ff91d6417bce0658c639
kissy24 <kissy105808j@gmail.com>
2023-02-16 00:23:06 +0900

    update fish settings

--- fish/config.fish
+++ fish/config.fish
@@ -6,8 +6,3 @@ set -g theme_nerd_fonts yes
 set -g theme_color_scheme nord
 # details
 set -g theme_date_format "+%F %H:%M"
-set -g theme_display_git_master_branch yes
-set -g theme_display_cmd_duration yes
-set -g theme_newline_cursor yes
-set -g theme_newline_prompt '$ '
-set -g fish_prompt_pwd_dir_length 0
