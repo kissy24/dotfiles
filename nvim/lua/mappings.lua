8772e0232aaccf0a85668d11ff96265f70be09c7
kissy24 <12809711+kissy24@users.noreply.github.com>
2023-02-24 09:31:14 +0900

    update keymapping

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -9,7 +9,8 @@ vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 -- terminal mode
-vim.keymap.set("n", "tm", "<cmd>belowright new<CR><cmd>terminal<CR>", { silent = true})
+vim.keymap.set("n", "tm", "<cmd>belowright new<CR><cmd>terminal<CR>", { silent = true })
+vim.keymap.set("t", "<ESC>", "<C-\\><C-n>")
 vim.cmd [[autocmd TermOpen * :startinsert]]
 vim.cmd [[autocmd TermOpen * setlocal norelativenumber]]
 vim.cmd [[autocmd TermOpen * setlocal nonumber]]
