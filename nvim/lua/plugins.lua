14cfc835f33ee3c4509b11142acc75f830b99068
kissy24 <12809711+kissy24@users.noreply.github.com>
2023-09-19 23:28:52 +0900

    pluginsとdefaultの設定を切り離しました

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -45,7 +45,8 @@ require "packer".startup(function(use)
     })
 end)
 
-require("coc")
+require("plugins.coc")
+require("plugins.mappings")
 require("lualine").setup()
 require("nvim-tree").setup()
 
