a6a27e0d4782d413793427dcc7b4661b7391b93e
kissy24 <kissy105808j@gmail.com>
2024-10-29 23:45:30 +0900

    feat: fishの体裁をstarshipに移行

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,2 @@
-# --- bobthefish settings ---
-# font 依存をなくす
-set -g theme_powerline_fonts no
-set -g theme_nerd_fonts yes
-# color scheme
-set -g theme_color_scheme nord
-# details
-set -g theme_date_format "+%F %H:%M"
+# starship 
+starship init fish | source
