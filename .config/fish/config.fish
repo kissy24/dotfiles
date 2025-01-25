6c9b9f9216be8b8119165aa080b51314040ee0d7
Kissy24 <Kissy24@Mac.AirPort>
2025-01-25 15:41:24 +0900

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,9 @@
 if type -q uv
     uv generate-shell-completion fish | source
 end
+# mac os
+if test (uname) = "Darwin"
+    eval (/opt/homebrew/bin/brew shellenv)
+end
 # starship 
 starship init fish | source
