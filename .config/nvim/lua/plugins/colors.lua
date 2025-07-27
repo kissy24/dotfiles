65624d2cec61a297b04a20f0973eca0211c2d10e
kissy24 <12809711+kissy24@users.noreply.github.com>
2025-07-27 11:30:38 +0900

    update lazy nvim settings

---
+++ .config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,25 @@
+return {
+    -- ColorScheme
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+    },
+
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd([[colorscheme tokyonight-moon]])
+        end,
+    },
+
+    {
+        'projekt0n/github-nvim-theme',
+        name = 'github-theme',
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+}
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,28 +1,4 @@
 return {
-    -- ColorScheme
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-    },
-
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.cmd([[colorscheme tokyonight-moon]])
-        end,
-    },
-
-    {
-        'projekt0n/github-nvim-theme',
-        name = 'github-theme',
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
-
     -- FileExplorer
     {
         "kyazdani42/nvim-tree.lua",
@@ -70,13 +46,6 @@ return {
             { "fb", "<cmd>lua require('telescope.builtin').buffers()<cr>",    mode = "n" },
             { "fn", "<cmd>lua require('telescope.builtin').help_tags()<cr>",  mode = "n" },
         }
-    },
-
-    -- Statusline
-    {
-        "nvim-lualine/lualine.nvim",
-        requires = { "kyazdani42/nvim-web-devicons" },
-        opts = {}
     },
 
     -- Treesitter
