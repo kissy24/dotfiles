0b7ab9529cdaa930efdf1dca129e1a7d5257127e
kissy24 <kissy105808j@gmail.com>
2025-10-12 12:41:52 +0900

    feat: add hotreload autocmd

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,9 @@
 require("base.options")
 require("base.mappings")
 require("base.lazy")
+
+-- 外部からファイルを変更されたら反映する
+vim.api.nvim_create_autocmd({ "WinEnter", "FocusGained", "BufEnter" }, {
+    pattern = "*",
+    command = "checktime",
+})
