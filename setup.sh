1d083c369981a4df010b1348fd7ff7645be41e5b
kissy24 <12809711+kissy24@users.noreply.github.com>
2025-11-06 23:36:12 +0900

    fix: lazygitがインストールできない問題の対処

--- setup.sh
+++ setup.sh
@@ -74,7 +74,7 @@ install_for_ubuntu() {
     curl -sS https://starship.rs/install.sh | sh -s -- -y
 
     echo "Installing lazygit..."
-    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name":' | sed -e 's/.*"v\([^"]*\)".*/\1/')
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
     tar xf lazygit.tar.gz lazygit
     sudo install lazygit /usr/local/bin
