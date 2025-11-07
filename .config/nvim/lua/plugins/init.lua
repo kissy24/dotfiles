c059504b8f6382e913a9386cf626a02b0b89ceb2
yuhei.kishida <yuhei.kishida@avantcorp.com>
2025-11-07 13:20:34 +0900

    feat: エディタの色付け調整

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -107,7 +107,35 @@ return {
     },
 
     -- Treesitter
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdateSync", opts = {} },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdateSync",
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+        end,
+        opts = {
+            ensure_installed = {
+                "bash",
+                "css",
+                "go",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "python",
+                "typescript",
+                "vim",
+                "yaml",
+            },
+            sync_install = false,
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+        },
+    },
 
     -- Markdown
     {
