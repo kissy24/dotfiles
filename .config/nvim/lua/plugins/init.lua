6fa6ac148fe4a327ae2bc63bf88f48a61421bcb3
kissy24 <12809711+kissy24@users.noreply.github.com>
2026-03-26 00:15:26 +0900

    feat: 使っていない機能を削除

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,23 +1,5 @@
 return {
     {
-        "akinsho/toggleterm.nvim",
-        version = "*",
-        opts = {
-            direction = "float",
-            float_opts = {
-                border = "rounded",
-                width = math.floor(vim.o.columns * 0.8),
-                height = math.floor(vim.o.lines * 0.7),
-                winblend = 0,
-            },
-        },
-        keys = {
-            { "tm",    "<cmd>ToggleTerm<cr>", mode = "n" },
-            { "<ESC>", "<C-\\><C-n>",         mode = "t" },
-        }
-    },
-
-    {
         "folke/noice.nvim",
         event = "VeryLazy",
         dependencies = {
