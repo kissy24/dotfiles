648b68cddf4715328b40191f5c92fb7fef0922ed
kissy24 <kissy105808j@gmail.com>
2026-03-26 00:15:26 +0900

    feat: 使っていない機能を削除

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,21 +1,6 @@
 return {
     -- ColorScheme
     {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-    },
-
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        -- config = function()
-        --     vim.cmd([[colorscheme tokyonight-moon]])
-        -- end,
-    },
-
-    {
         'projekt0n/github-nvim-theme',
         name = 'github-theme',
         lazy = false,
@@ -28,16 +13,5 @@ return {
             })
             vim.cmd([[colorscheme github_dark_dimmed]])
         end,
-    },
-
-    {
-        'rebelot/kanagawa.nvim',
-        name = 'kanagawa',
-        lazy = false,
-        priority = 1000,
-        opts = {},
-        -- config = function()
-        --     vim.cmd([[colorscheme kanagawa-dragon]])
-        -- end,
     },
 }
