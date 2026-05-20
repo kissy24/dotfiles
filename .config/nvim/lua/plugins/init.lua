100df2b02e735d1006922765afa600e3b9db838b
kissy24 <12809711+kissy24@users.noreply.github.com>
2026-05-20 22:27:23 +0900

    feat: プラグイン数を減らしました

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,12 +1,4 @@
 return {
-    { "windwp/nvim-autopairs", config = true, lazy = false },
-    {
-        'MeanderingProgrammer/render-markdown.nvim',
-        dependencies = { 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
-        ---@module 'render-markdown'
-        ---@type render.md.UserConfig
-        opts = {},
-    },
     {
         'projekt0n/github-nvim-theme',
         name = 'github-theme',
@@ -20,5 +12,23 @@ return {
             })
             vim.cmd([[colorscheme github_dark_dimmed]])
         end,
+    },
+    { "lewis6991/gitsigns.nvim", opts = {} },
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = { "LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile" },
+        dependencies = { "nvim-lua/plenary.nvim" },
+        keys = { { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" } },
+    },
+    {
+        "f-person/git-blame.nvim",
+        event = "VeryLazy",
+        opts = {
+            enabled = true,
+            message_template = " <summary> • <date> • <author> • <<sha>>",
+            date_format = "%Y-%m-%d %H:%M:%S",
+            virtual_text_column = 1,
+        }
     },
 }
